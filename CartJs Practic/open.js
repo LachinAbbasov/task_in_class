6f335f6ad16361dbc1e7d870336bc38495ff83da
LachinAbbasov <tu7n8gcpt@code.edu.az>
2024-04-17 13:36:57 +0400

    halftask

--- CartJs Practic/open.js
+++ CartJs Practic/open.js
@@ -1,5 +1,9 @@
 const api_link = "https://fakestoreapi.com/products";
 id = new URLSearchParams(window.location.search).get("id");
-fetch(api_link + `/${id}`).then((res) => res.json().then(
-    
-));
+fetch(api_link + `/${id}`)
+.then((res) => res.json())
+.then((data)=>console.log(data.title)
+
+
+
+);
